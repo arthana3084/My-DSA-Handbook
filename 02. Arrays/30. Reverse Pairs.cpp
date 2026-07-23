923e5bbc66dbe2d65ca3200bf6393dee435f653a
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-07-23 15:05:49 +0530

    Create 30. Reverse Pairs.cpp

---
+++ 02. Arrays/30. Reverse Pairs.cpp
@@ -0,0 +1,37 @@
+/*
+==================================================
+Problem      : Reverse Pairs
+Platform     : LeetCode 493
+
+Difficulty   : Hard
+
+Pattern      : Merge Sort
+
+Time          : O(n log n)
+Space         : O(n)
+
+Key Idea
+---------
+Count valid pairs before merging.
+
+Status              : ⭐ Must Revise
+Frequently Asked    : ⭐⭐⭐⭐⭐
+==================================================
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int reversePairs(vector<int>& nums)
+{
+    // Merge Sort based solution
+
+    return 0;
+}
+
+int main()
+{
+    cout<<"Refer Merge Sort implementation.";
+
+    return 0;
+}
