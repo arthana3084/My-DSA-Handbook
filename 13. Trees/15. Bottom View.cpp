7965d0578f800c3f71b565fa3d12f5caf72b6d38
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-07-23 20:19:32 +0530

    Create 15. Bottom View.cpp

---
+++ 13. Trees/15. Bottom View.cpp
@@ -0,0 +1,76 @@
+/*
+==================================================
+Problem      : Bottom View of Binary Tree
+Platform     : GFG
+Difficulty   : Medium
+
+Pattern       : BFS
+
+Time          : O(n log n)
+Space         : O(n)
+
+Status              : ⭐ Must Revise
+Frequently Asked    : ⭐⭐⭐⭐⭐
+==================================================
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left,*right;
+
+    Node(int x)
+    {
+        data=x;
+        left=right=NULL;
+    }
+};
+
+class Solution
+{
+public:
+
+    vector<int> bottomView(Node *root)
+    {
+        vector<int> ans;
+
+        if(root==NULL)
+            return ans;
+
+        map<int,int> mp;
+
+        queue<pair<Node*,int>> q;
+
+        q.push({root,0});
+
+        while(!q.empty())
+        {
+            auto it=q.front();
+            q.pop();
+
+            Node* node=it.first;
+            int line=it.second;
+
+            mp[line]=node->data;
+
+            if(node->left)
+                q.push({node->left,line-1});
+
+            if(node->right)
+                q.push({node->right,line+1});
+        }
+
+        for(auto it:mp)
+            ans.push_back(it.second);
+
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
