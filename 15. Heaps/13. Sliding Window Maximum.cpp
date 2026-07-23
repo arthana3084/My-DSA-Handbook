c0758cb972684f9e0f0b3650ea2fb3c412efed99
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-07-23 20:43:08 +0530

    Create 13. Sliding Window Maximum.cpp

---
+++ 15. Heaps/13. Sliding Window Maximum.cpp
@@ -0,0 +1,51 @@
+/*
+==================================================
+Problem      : Sliding Window Maximum
+Platform     : LeetCode 239
+
+Difficulty   : Hard
+
+Pattern      : Monotonic Deque
+
+Time          : O(n)
+Space         : O(k)
+
+Status              : ⭐ Must Revise
+Frequently Asked    : ⭐⭐⭐⭐⭐
+==================================================
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+
+    vector<int> maxSlidingWindow(vector<int>& nums,int k)
+    {
+        deque<int> dq;
+        vector<int> ans;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            while(!dq.empty() && dq.front()<=i-k)
+                dq.pop_front();
+
+            while(!dq.empty() && nums[dq.back()]<=nums[i])
+                dq.pop_back();
+
+            dq.push_back(i);
+
+            if(i>=k-1)
+                ans.push_back(nums[dq.front()]);
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
