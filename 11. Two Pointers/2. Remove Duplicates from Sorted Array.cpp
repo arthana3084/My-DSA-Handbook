68aae9e680f0aa4efa25c20ba79788b29c6f5efc
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-07-23 20:46:27 +0530

    Create 2. Remove Duplicates from Sorted Array.cpp

---
+++ 11. Two Pointers/2. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,45 @@
+/*
+==================================================
+Problem      : Remove Duplicates from Sorted Array
+Platform     : LeetCode 26
+
+Difficulty   : Easy
+
+Pattern      : Two Pointers
+
+Time          : O(n)
+Space         : O(1)
+==================================================
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+
+    int removeDuplicates(vector<int>& nums)
+    {
+        if(nums.empty())
+            return 0;
+
+        int i=0;
+
+        for(int j=1;j<nums.size();j++)
+        {
+            if(nums[j]!=nums[i])
+            {
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+
+        return i+1;
+    }
+};
+
+int main()
+{
+    return 0;
+}
