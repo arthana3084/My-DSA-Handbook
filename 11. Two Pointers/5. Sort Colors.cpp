59c42b5496c3c932d867bd7c1c99364847a685d8
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-07-23 20:47:25 +0530

    Create 5. Sort Colors.cpp

---
+++ 11. Two Pointers/5. Sort Colors.cpp
@@ -0,0 +1,46 @@
+/*
+==================================================
+Problem      : Sort Colors
+Platform     : LeetCode 75
+
+Difficulty   : Medium
+
+Pattern      : Dutch National Flag
+
+Time          : O(n)
+Space         : O(1)
+
+Status              : ⭐ Must Revise
+Frequently Asked    : ⭐⭐⭐⭐⭐
+==================================================
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+
+    void sortColors(vector<int>& nums)
+    {
+        int low=0,mid=0,high=nums.size()-1;
+
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+                swap(nums[low++],nums[mid++]);
+
+            else if(nums[mid]==1)
+                mid++;
+
+            else
+                swap(nums[mid],nums[high--]);
+        }
+    }
+};
+
+int main()
+{
+    return 0;
+}
