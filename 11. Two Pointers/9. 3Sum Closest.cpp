022ffe0aa7e8ed3b51ddd339400ce73d916f1aa3
Arthana Sreekesh <146968293+arthana3084@users.noreply.github.com>
2026-07-23 20:49:10 +0530

    Create 9. 3Sum Closest.cpp

---
+++ 11. Two Pointers/9. 3Sum Closest.cpp
@@ -0,0 +1,59 @@
+/*
+==================================================
+Problem      : 3Sum Closest
+Platform     : LeetCode 16
+
+Difficulty   : Medium
+
+Pattern      : Sorting + Two Pointers
+
+Time          : O(n²)
+Space         : O(1)
+
+Status              : ⭐ Must Revise
+Frequently Asked    : ⭐⭐⭐⭐☆
+==================================================
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+
+    int threeSumClosest(vector<int>& nums,int target)
+    {
+        sort(nums.begin(),nums.end());
+
+        int ans=nums[0]+nums[1]+nums[2];
+
+        for(int i=0;i<nums.size()-2;i++)
+        {
+            int left=i+1;
+            int right=nums.size()-1;
+
+            while(left<right)
+            {
+                int sum=nums[i]+nums[left]+nums[right];
+
+                if(abs(target-sum)<abs(target-ans))
+                    ans=sum;
+
+                if(sum<target)
+                    left++;
+                else if(sum>target)
+                    right--;
+                else
+                    return target;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    return 0;
+}
